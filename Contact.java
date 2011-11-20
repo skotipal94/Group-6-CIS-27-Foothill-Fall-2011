c6dfd238aa9fa40974407337cfed731730d8cf1f
Robert Edwards <doesnt.remember@gmail.com>
2011-11-19 19:51:58 -0800

    list of getValue correspondence added

--- Contact.java
+++ Contact.java
@@ -17,7 +17,19 @@ public class Contact {
 	
 	//return a chosen variable based on integer passed in
 	public String getValue(int choice){
-		
+		/*
+		 *list of choice correspondence:
+		 *1-first name
+		 *2-last name
+		 *3-middle initial
+		 *4-phone number
+		 *5-street address
+		 *6-city
+		 *7-state
+		 *8-zipcode
+		 *9-email address
+		 *10-notes
+		*/
 		//put switch here to determine return value
 		System.out.println("getValue() called");
 		return "some string";
