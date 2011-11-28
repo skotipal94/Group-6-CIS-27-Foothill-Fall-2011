621b1ede1b2b2a99adbee0da99ef3c2dc0daa9de
Noah Azarin <blazinghand@gmail.com>
2011-11-27 17:30:40 -0800

    added comparator, initials, and filled otu sort and addcontact methods
    this is for task 3

--- ContactList.java
+++ ContactList.java
@@ -1,71 +1,88 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class ContactList {
 
 	/**
 	 this class contains functions/methods for interacting with a list of Contact objects.
 	 these functions will be called in Main
-	 THIS IS CURRENTLY PRELIMINARY CODE. THIS WILL COMPILE BUT THE FUNCTIONS CURRENTLY DO NOTHING.
+	 THIS IS CURRENTLY PRELIMINARY CODE.
+	 NA
 	 */
+
+	private Contact[] contactList = new Contact[100]; //this is an array to hold up to 100 contact objects
+	private String temp;//test variable, remove for final
+	private Contact nextContact = new Contact();//test object, dot operator will run from contactList, remove for final
+	private int contactListPointer = 0;
 	
-	private String[] singleEntry; // this string[] is for single contact entry, including name, number, etc
-	private ArrayList<String[]> totalList; //this ArrayList<String[]> is a list of several Contact objects, and represents our contact list. 
-	private ArrayList<String[]> subList; //this ArrayList<String[]> is list of several entries, often a subset of the contact list, used for searches.
-	
-	public void addContact(String firstname, String middleInitial, String lastName, String phoneNumber, String streetAddress, String city, String state, String zip, String email){
+	public void addContact(String firstName, String middleInitial, String lastName, String phoneNumber, String streetAddress, String city, String state, String zip, String email, String notes){
 		/*
 		 * This function adds a new contact to the contact list. 
-		 * parameters: several strings that contain contact informatino.
+		 * parameters: several strings that contain contact information.
 		 * returns: nothing
-		 */
-		//Contact nextContact = new Contact
-		//setValue(firstName, middleInitial, lastName, phoneNumber, streetAddress, city, state, zip, email);
-		System.out.println("addContact called"); //debug
+		 * NA
+		 */		
+		System.out.println("addContact called"); //debug	
+		nextContact.setValue(firstName, middleInitial, lastName, phoneNumber, streetAddress, city, state, zip, email, notes);
+		contactList[contactListPointer] = nextContact;
+		contactListPointer++;
 	}
 	
+  public static Comparator LastNameComparator = new Comparator() {
+	    public int compare(Object person, Object anotherPerson) {
+	      String lastName1 = ((contactList) person).getLastName();
+	      String lastName2 = ((contactList) anotherPerson).getLastName();
+	      return lastName1.compareTo(lastName2);
+	    }
+	  };
+	  
 	public void sort(){
 		/*
 		 * this function sorts the existing contact list by last name
 		 * parameters: none
 		 * returns: nothing
+		 * NA
 		 */
-		//getValue(lastName);
+		//sort loop here
 		System.out.println("sort called"); //debug
+	    Arrays.sort(contactList, Contact.LastNameComparator);
+	    System.out.println();
+	    System.out.println("Sorted by last name");
+
 	}
 	
-	public ArrayList<String[]> printList(){
+	public String returnList(){
 		/*
 		 * This function returns the entire list. it DOES NOT print the list. call this function and use its output to print.
 		 * parameters: none
-		 * returns: entire list in an ArrayList of String[]
+		 * returns: a String built from the contactList
 		 */
-		//getValue();
+		//loop the list and load to String here
 		System.out.println("printList called"); //debug
-		return totalList;
+		return "some String";//return loaded String
 	}
 	
-	public ArrayList<String[]> lastNameSearch(String email){
+	public String lastNameSearch(String lastName){
 		/*
 		 * This function searches the list for a specific last name.
-		 * parameters: email address as a string
-		 * returns: an ArrayList of String[]. This will usually have only one element, a single String[] corresponding to a single entry.
-		 * sometimes, though, it'll have more than one element since people will share a last name, which is why it's an ArrayList<String[]>
-		 * instead of just a String[]. 
+		 * parameters: last name as a string
+		 * returns: a String built from all found last names
 		 */
-		//getvalue();
+		//loop through the contactList here, pulling the last name from each contact, build the return String
+		temp = nextContact.getValue(2);//2 is the last name code for the Contact switch
 		System.out.println("lastNameSearch called"); //debug
-		return subList;
+		return "some String";
 	}
 	
-	public ArrayList<String[]> zipSearch(String zip){
+	public String zipSearch(String zip){
 		/*
 		 * This function searches the list for a specific zip code.
 		 * parameters: zip code as a STRING. not as an int. as a STRING.
-		 * returns: an ArrayList of String[]. This can have one or more elements, with a single String[] corresponding to a single entry.
+		 * returns: a String, built from all found zipcodes.
 		 * the reason it is an ArrayList is because people share zip codes.
 		 */
-		//getValue();
+		//loop thourgh the contactList here, pulling zip for each contact build the return String
+		temp = nextContact.getValue(8);//8 is the zipcode code for the Contact switch
 		System.out.println("zipSearch called"); //debug
-		return subList;
+		return "some String";
 	}
 }
